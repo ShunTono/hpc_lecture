b49960bb42af64fe4ea324cf15134f4e4a084a90
ShunTono <64898062+ShunTono@users.noreply.github.com>
2020-05-16 23:44:05 +0900

    Add files via upload

--- 11_distributed.cpp
+++ 11_distributed.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv){
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  Body ibody[N/size], jbody[N/size], jbody_recv[N/size];
   srand48(rank);
   for(int i=0; i<N/size; i++){
     ibody[i].x = jbody[i].x = drand48();
@@ -28,12 +28,13 @@ int main(int argc, char** argv){
   MPI_Type_commit(&MPI_BODY);
   for(int irank=0; irank<size; irank++){
     MPI_Win win;
-    MPI_Win_create(jbody, N/size*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_create(jbody_recv, N/size*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     MPI_Win_fence(0, win);
     MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
     MPI_Win_fence(0, win);
     for(int i=0; i<N/size; i++){
       for(int j=0; j<N/size; j++){
+	jbody[j] = jbody_recv[j];
 	double rx = ibody[i].x - jbody[j].x;
 	double ry = ibody[i].y - jbody[j].y;
 	double r = std::sqrt(rx * rx + ry * ry);
